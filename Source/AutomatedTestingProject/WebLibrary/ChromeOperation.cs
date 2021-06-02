48892407579e82d81ed8ed90c1f16c21680f6746
cookiedog <iverylikedog@gmail.com>
2021-06-02 18:44:49 +0800

    1.修正上傳部份資料遺失錯誤

---
+++ Source/AutomatedTestingProject/WebLibrary/ChromeOperation.cs
@@ -0,0 +1,51 @@
+﻿using BasicLIbrary;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebLibrary
+{
+    public class DriverOperation : Chrome
+    {
+        public DriverOperation(CommonBase basicTool, Category type) : base(basicTool, type)
+        {
+
+        }
+
+        public bool RestartDriver()
+        {
+            try
+            {
+                ChromeOptions options = new ChromeOptions();
+                if (singletonWebDriver == null)
+                {
+                    singletonWebDriver = new OpenQA.Selenium.Chrome.ChromeDriver();
+                    singletonWebDriver.Manage().Window.Maximize();
+                }
+                else
+                {
+                    singletonWebDriver.Quit();
+                    singletonWebDriver = null;
+                    singletonWebDriver = new OpenQA.Selenium.Chrome.ChromeDriver();
+                    singletonWebDriver.Manage().Window.Maximize();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                basicTool.messageLog.WriteLog(Category.DriverOperation, ex.ToString(), "RestartDriver");
+                return false;
+            }
+        }
+        public bool isDriverAlive()
+        {
+            if (singletonWebDriver == null)
+                return false;
+            else
+                return true;
+        }
+    }
+}
